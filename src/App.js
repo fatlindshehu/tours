4d9556d3534c99bf9713ef937a5160a46929e46b
Fatlind Shehu <fatlindshehu4@gmail.com>
2022-05-06 22:23:23 +0100

    Showing a particular number of words on the paragraphs

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import Tours from "./Tours";
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
-
   const fetchTours = async () => {
     setLoading(true);
     try {
--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt="name" />
       <footer>
         <div className="tour-info">
           <h4>{name}</h4>
-          <h4 className="tour-price">${price}</h4>
+          <h4 className="tour-price">${price}...</h4>
         </div>
-        <p>{info}</p>
+        <p>{readMore ? info : `${info.substring(0, 200)}`}</p>
         <button className="delete-btn">Not interested</button>
       </footer>
     </article>
